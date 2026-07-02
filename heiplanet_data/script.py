8e4791b98a0b2cd22b61fca3dce145a9de85c1d3
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2026-07-02 09:16:00 +0200

    Maintenance: integration and documentation updates (#126)
    * update: update script and notebooks for updated interfaces
    
    * fix: adapt preprocessing and output folder in script
    
    * fix: harmonize documentation and tutorials with changes
    
    * fix: do not expand secret alongside third-party actions
    
    * CI: install pip for integration test ci
    
    * CI: comment out integration tests for now

--- heiplanet_data/script.py
+++ heiplanet_data/script.py
@@ -1,12 +1,10 @@
+import time
 from pathlib import Path
 from heiplanet_data.inout import (
     download_data,
     get_filename,
 )
-from heiplanet_data.preprocess import (
-    preprocess_data_file,
-)
-from heiplanet_data import utils
+from heiplanet_data import preprocess
 
 if __name__ == "__main__":
     # get the era5 land data for 2016 and 2017
@@ -17,34 +15,25 @@ if __name__ == "__main__":
     dataset = "reanalysis-era5-land-monthly-means"
     request = {
         "product_type": ["monthly_averaged_reanalysis"],
-        "variable": ["2m_temperature", "total_precipitation"],
-        "year": ["2016", "2017"],
+        "variable": ["2m_temperature"],
+        "year": ["2025"],
         "month": [
-            "01",
-            "02",
-            "03",
-            "04",
             "05",
             "06",
             "07",
-            "08",
-            "09",
-            "10",
-            "11",
-            "12",
         ],
         "time": ["00:00"],
         "data_format": data_format,
         "download_format": "unarchived",
     }
     file_name = get_filename(
-        dataset,
-        data_format,
-        request["year"],
-        request["month"],
-        "area" in request,
-        "era5_data",
-        request["variable"],
+        ds_name=dataset,
+        data_format=data_format,
+        years=request["year"],
+        months=request["month"],
+        has_area=False,
+        base_name="era5_data",
+        variables=request["variable"],
     )
     output_file = data_folder / file_name
 
@@ -54,32 +43,15 @@ if __name__ == "__main__":
     else:
         print("Data already exists at {}".format(output_file))
 
-    settings = utils.get_settings(
-        setting_path="default",
-        new_settings={},
-        updated_setting_dir=None,
-        save_updated_settings=False,
-    )
-
-    # disable truncation of dates
-    settings["truncate_date"] = False
-    print("Preprocessing ERA5-Land data...")
-    preprocessed_dataset = preprocess_data_file(
+    print(f"Preprocessing ERA5-Land data: {output_file}")
+    t0 = time.time()
+    preprocessed_dataset, era5_pfname = preprocess.preprocess_data_file(
         netcdf_file=output_file,
-        settings=settings,
+        source="era5",
+        settings="default",
+        new_settings={"output_dir": str(data_folder_out)},
+        unique_tag="silver",
     )
-    # here we need to provide output folder
-    # preprocess the population data
-    popu_file = data_folder / "population_histsoc_30arcmin_annual_1901_2021_renamed.nc"
-    settings["truncate_date"] = True
-    # disable uncessary preprocessing steps
-    settings["adjust_longitude"] = False
-    settings["convert_kelvin_to_celsius"] = False
-    settings["convert_m_to_mm_precipitation"] = False
-    settings["resample_grid"] = False
-
-    print("Preprocessing population data...")
-    preprocessed_popu = preprocess_data_file(
-        netcdf_file=popu_file,
-        settings=settings,
-    )
+    t_preprocess = time.time()
+    print(f"Preprocessing completed in {t_preprocess - t0:.2f} seconds.")
+    print(f"Name of preprocessed file: {era5_pfname}")
